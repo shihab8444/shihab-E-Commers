8fd281cdde0efa8bc715de6dbd8b0b3e094fb85b
shihab8444 <shihab8444@gmail.com>
2022-11-10 01:56:59 +0600

    changed

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>contuct us</div>
+          <div className='right row RText'>
+            <label>FAQ</label>
+            <label>Need Help?</label>
+            <label>BD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
